7445779b90b8aa244520a9647b8423ed01fa92c8
gustavoblasques <gustavo.blasques05@gmail.com>
2026-07-07 16:22:38 -0300

    Added logs for downloaded manga.

--- core.py
+++ core.py
@@ -1,6 +1,7 @@
 import input_handler
 from services.mangadex_api import buscar_id_por_titulo, manga_info, get_chapter_link, get_chapter_feed
 from services.chapter_downloader import create_folder_download
+from services.log_manager import load_log, check_log, register_download_log
 import sys
 
 
@@ -8,6 +9,7 @@ import sys
 def main():
     while True:
         lista_busca = []
+        log = load_log()
         try:
             welcome_option = input_handler.escolha_usuario_inicio()
             if welcome_option == 1:
@@ -22,16 +24,28 @@ def main():
                 input_chapters = input_handler.choose_chapters()
                 capitulos = []
                 if input_chapters is None:
-                    get_chapter_link(cap.chapter_id)
-                    create_folder_download(manga_final.title,cap.number,cap.title,link)
+                    for cap in chapter_feed:
+                        if manga_final.manga_id not in log or cap.chapter_id not in log[manga_final.manga_id]:
+                            get_chapter_link(cap.chapter_id)
+                            create_folder_download(manga_final.title,cap.number,cap.title,link)
+                            register_download_log(manga_final.manga_id, cap.chapter_id)
+                        else:
+                            print(f"Capítulos {cap.chapter_id} já baixados. Pulando...")
                 else:
-                    for i in input_chapters:
-                        capitulos.append(chapter_feed[i - 1])
+                    try:
+                        for i in input_chapters:
+                            capitulos.append(chapter_feed[i - 1])
 
-                    for cap in capitulos:
-                        print(cap.chapter_id)
-                        link = get_chapter_link(cap.chapter_id)
-                        create_folder_download(manga_final.title,cap.number,cap.title,link)
+                        for cap in capitulos:
+                            if manga_final.manga_id not in log or cap.chapter_id not in log[manga_final.manga_id]:
+                                print(cap.chapter_id)
+                                link = get_chapter_link(cap.chapter_id)
+                                create_folder_download(manga_final.title,cap.number,cap.title,link)
+                                register_download_log(manga_final.manga_id, cap.chapter_id)
+                            else:
+                                print(f"Capítulo(s) {cap.chapter_id} já baixado(s). Pulando...")
+                    except IndexError:
+                        print(f"Capítulos não encontrados. Certifique-se que os capítulos estão disponíveis.")
 
             elif welcome_option == 2:
                 choose_lang = input_handler.manga_language
@@ -54,6 +68,7 @@ def manga_select(lista_busca):
 
 
 if __name__ == "__main__":
+    check_log()
     main()
 
 
